f432f6d7202fca523b5f055bcb57d6432efea2cc
TatianaGerasimova2022 <106173658+TatianaGerasimova2022@users.noreply.github.com>
2022-05-24 20:05:02 +0300

    change3

--- venv/10.py
+++ venv/10.py
@@ -8,6 +8,7 @@ def get_weather_by_city(city_name):
 print(get_weather_by_city('Praha'))
 print(get_weather_by_city('Praha123'))
 print(get_weather_by_city('Praha123456'))
+print(get_weather_by_city('Praha1234568910'))
 # print(weather)
 # print(type(weather))
 # print(weather.keys())
